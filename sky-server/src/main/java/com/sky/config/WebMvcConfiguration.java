14b5d0f3c45b5a1532431a83a2624c44e54e9e3b
steven <smith2005qaz@outlook.com>
2025-04-14 21:17:17 +0800

    添加购物车业务代码开发

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -113,7 +113,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     @Override
     protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         log.info("扩展消息转换器...");
-//        创建一个消息转换器帝乡
+//        创建一个消息转换器对象
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
 //        为消息转换器设置一个对象转换器,对象转换器可以将Java对象序列化为json数据
         converter.setObjectMapper(new JacksonObjectMapper());
