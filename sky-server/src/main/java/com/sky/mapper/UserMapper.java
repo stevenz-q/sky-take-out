3581f974405cde452815bdf89ae7ae4a95958c78
steven <smith2005qaz@outlook.com>
2025-04-18 10:32:33 +0800

    订单支付功能代码

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -17,7 +17,17 @@ public interface UserMapper {
 
     /**
      * 插入数据
+     *
      * @param user
      */
     void insert(User user);
+
+    /**
+     * 根据id查询用户
+     *
+     * @param userId
+     * @return
+     */
+    @Select("select * from user where id = #{id}")
+    User getById(Long userId);
 }
