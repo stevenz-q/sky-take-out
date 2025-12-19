9303bc333a195e8cb12ca87a302d514b8a4d99d1
Steven_Z <Smith2005qaz@outlook.com>
2025-12-19 21:27:54 +0800

    整合前端后端微信小程序段

---
+++ sky-admin/sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -0,0 +1,117 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 套餐管理
+ */
+@RestController
+@RequestMapping("/admin/setmeal")
+@Slf4j
+@Api(tags = "套餐相关接口")
+public class SetmealController {
+
+    @Autowired
+    private SetmealService setmealService;
+
+    /**
+     * 新增套餐
+     *
+     * @param setmealDTO
+     * @return
+     */
+    @CacheEvict(cacheNames = "setmealCache", key = "#setmealDTO.categoryId")
+    @PostMapping
+    @ApiOperation("新增套餐")
+    public Result<String> save(@RequestBody SetmealDTO setmealDTO) {
+        log.info("新增套餐:{}", setmealDTO);
+        setmealService.saveWithDish(setmealDTO);
+        return Result.success();
+    }
+
+    /**
+     * 套餐分页查询
+     *
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("套餐分页查询")
+    public Result<PageResult> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+        log.info("套餐分页查询:{}", setmealPageQueryDTO);
+        PageResult pageResult = setmealService.pageQuery(setmealPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 批量删除套餐
+     *
+     * @param ids
+     * @return
+     */
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
+    @DeleteMapping
+    @ApiOperation("批量删除套餐")
+    public Result<String> delete(@RequestParam List<Long> ids) {
+        log.info("批量删除套餐:{}", ids);
+        setmealService.deleteBatch(ids);
+        return Result.success();
+    }
+
+    /**
+     * 根据id查询套餐
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询套餐")
+    public Result<SetmealVO> getById(@PathVariable Long id) {
+        log.info("根据id查询套餐:{}", id);
+        SetmealVO setmealVO = setmealService.getByIdWithDish(id);
+        return Result.success(setmealVO);
+    }
+
+    /**
+     * 修改套餐
+     *
+     * @param setmealDTO
+     * @return
+     */
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
+    @PutMapping
+    @ApiOperation("修改套餐")
+    public Result<String> update(@RequestBody SetmealDTO setmealDTO) {
+        log.info("修改套餐:{}", setmealDTO);
+        setmealService.update(setmealDTO);
+        return Result.success();
+    }
+
+    /**
+     * 套餐起售停售
+     *
+     * @param status
+     * @param id
+     * @return
+     */
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
+    @PostMapping("/status/{status}")
+    @ApiOperation("套餐起售停售")
+    public Result<String> startOrStop(@PathVariable Integer status, Long id) {
+        setmealService.startOrStop(status, id);
+        return Result.success();
+    }
+}
